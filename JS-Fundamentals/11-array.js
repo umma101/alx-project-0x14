c5fe95af231029386a3758448b3d2fa02c124512
Mary <maryauma2606@gmail.com>
2025-05-24 15:38:50 +0300

    Add my JavaScript amazing script

---
+++ JS-Fundamentals/11-array.js
@@ -0,0 +1,5 @@
+let apples = 4;
+let oranges = 5;
+let totalFruits = apples + oranges;
+
+console.log("I have " + apples + " apples, " + oranges + " oranges, and " + totalFruits + " total fruits.");
